46d078a3872f3a086c867ac5d39e83595829b4ff
Chris Aquino <chris@radishmouse.com>
2019-12-22 21:17:18 -0500

    adds solution

--- index.js
+++ index.js
@@ -12,6 +12,25 @@ const server = http.createServer((browserReq, serverResp) => {
         url = '/index.html';
     }
 
+    let isImage = false;
+    let contentType = 'text/html';
+    if (url.endsWith('css')) {
+      console.log('css');
+      contentType = 'text/css';
+    } else if (url.endsWith('jpg') || url.endsWith('jpeg')) {
+      isImage = true;
+      console.log('jpg');
+      contentType = 'image/jpg';
+    } else if (url.endsWith('png')) {
+      isImage = true;
+      console.log('png');
+      contentType = 'image/png';
+    } else if (url.endsWith('gif')) {
+      isImage = true;
+      console.log('gif');
+      contentType = 'image/gif';
+    }
+
     // Read the HTML file's contents
     // so we can send it in the Response
     fs.readFile(`${__dirname}/public${url}`, (error, buf) => {
@@ -19,17 +38,22 @@ const server = http.createServer((browserReq, serverResp) => {
             console.log(error);
 
             serverResp.writeHead(404, {
-                'Content-Type': 'text/html'
+              'Content-Type': 'text/html'
             });
 
             serverResp.end(`<h1>File not found</h1>`);
         } else {
             serverResp.writeHead(200, {
-                'Content-Type': 'text/html'
+                // 'Content-Type': 'text/html'
+                'Content-Type': contentType
                 // 'Content-Type': 'application/json'
             });
-            const contents = buf.toString();
-            serverResp.end(contents);
+            if (isImage) {
+              serverResp.end(buf);
+            } else {
+              const contents = buf.toString();
+              serverResp.end(contents);
+            }
         }
     });
     // serverResp.end(`<h1>OMG IT WORKS!!!!</h1>`);
